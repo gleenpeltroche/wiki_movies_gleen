824f66e9b517c015080c761d620138aaf198f0d5
Renato De la Rosa <srdelarosab@gmail.com>
2022-10-24 22:05:06 -0500

    Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrivateGuard } from './core/guards/private.guard';
+import { PublicGuard } from './core/guards/public.guard';
 import { LayoutComponent } from './core/layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
@@ -9,14 +11,17 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [PrivateGuard],
   },
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [PublicGuard],
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [PublicGuard],
   },
   {
     path: '**',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -37,5 +37,9 @@ export class LoginComponent implements OnInit {
       },
     });
   }
+
+  redirectToRegister() {
+    this.router.navigateByUrl('/register');
+  }
   ngOnInit(): void {}
 }
